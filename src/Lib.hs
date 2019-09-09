43cf7b010f88df7e0f2ab58412d98afcbf7b270e
Malcolm Mulong <randomthought@architect>
2019-09-08 20:44:07 -0700

    added capability to scrape links from urls

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,26 @@
-module Lib where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Arrows #-}
+
+module Lib (
+  UrlsToProcess
+, WorkerCount
+, ProcessedHashes
+, worker
+) where
 
 import Prelude hiding (putStrLn)
 import Data.String.Class (putStrLn)
+import Data.Text.Internal (showText)
 import Control.Concurrent
 import Control.Concurrent.STM
-import Network.HTTP.Conduit
+import Network.Curl
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Control.Monad.Loops
-
-type Url = String
+import Text.XML.HXT.Core
+import Text.XML.HXT.Curl
+import Text.XML.HXT.TagSoup
+import Text.HandsomeSoup
 
 type ProcessedHashes = TVar (Set.Set String)
 
@@ -19,16 +30,14 @@ type Links = Int
 
 type WorkerCount = TVar Int
 
-data UrlData = UrlData {hash :: String, url :: Url}
+data UrlData = UrlData {hash :: String, url :: URLString}
   deriving (Show, Eq)
 
+-- stringToHash :: String -> UrlData
+-- stringToHash =  error "Not implemented"
 
-stringToHash :: String -> UrlData
-stringToHash =  error "Not implemented"
-
-writeResults :: UrlData -> FilePath -> IO ()
-writeResults _ _ = putStrLn "Do"
-
+-- writeResults :: UrlData -> FilePath -> IO ()
+-- writeResults _ _ = putStrLn "Do"
 
 worker :: UrlsToProcess -> ProcessedHashes -> WorkerCount -> IO Int
 worker queue processed workerCount =
@@ -46,8 +55,33 @@ worker queue processed workerCount =
         return n'
   in iterateUntilM lessThan5 work 0
 
-processUrl:: ProcessedHashes -> Url -> IO Links
-processUrl _ _ = do
+processUrl:: ProcessedHashes -> URLString -> IO Links
+processUrl _ url = do
+  let curlOptions = [CurlTimeout 3, CurlFollowLocation True, CurlMaxRedirs 5]
+  (code, body) <- curlGetString url curlOptions
+  l <- extractLinks body
+  print l
   threadid <- myThreadId
-  putStrLn $ "running: " ++ (show threadid)
+  putStrLn $ "Url: " ++ url ++ "\nResponse: " ++ show code ++ "\nWith Thread: " ++ show threadid ++ "\n"
   return (0 :: Links)
+
+type Selector = String
+type PageData = String
+
+extractContents :: String -> [Selector] -> [PageData]
+extractContents body s =  error "Need to implement"
+
+-- extractLinks :: String -> [URLString]
+-- extractLinks c = error "Implement"
+
+extractLinks :: String -> IO [URLString]
+extractLinks body = extract (readString
+  [ withParseHTML yes, withWarnings no
+  ] body)
+
+extract doc = runX $ doc //> hasName "a" >>> getAttrValue "href"
+-- extract doc = runX $ doc >>> xmlFilter "article" >>> xmlFilter "a" >>> toHref
+
+-- toHref = proc el -> do
+--    link    <- getAttrValue "href" -< el
+--    returnA -< link
